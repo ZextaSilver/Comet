82a98f0e3a38e836e108480eae6cdd0e02edda41
Bowen Zhao <bzhao19@ucsc.edu>
2020-10-27 16:28:22 -0400

    Coment

---
+++ Script/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour{
+
+    public float movement_speed;
+    public Rigidbody2D rigid_body;
+
+    float move_x;
+
+    private void Update(){
+
+        move_x = Input.GetAxisRaw("Horizontal");
+
+    }
+
+    public void FixedUpdate(){
+
+        Vector2 movement = new Vector2(move_x * movement_speed, rigid_body.velocity.y);
+
+        rigid_body.velocity = movement;
+        
+    }
+}
