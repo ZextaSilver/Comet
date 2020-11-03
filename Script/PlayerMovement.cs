fe8c5978ccf1f497a6d9631418b2387b1def7999
Bowen Zhao <bzhao19@ucsc.edu>
2020-11-03 18:53:49 -0500

    character move left/right with sprite flipping

--- Script/PlayerMovement.cs
+++ Script/PlayerMovement.cs
@@ -4,23 +4,49 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour{
 
-    public float movement_speed;
-    public Rigidbody2D rigid_body;
+    public float move_speed;
+    private bool facing_right = true;
+    private float move_x;
 
-    float move_x;
+    void Update(){
 
-    private void Update(){
+        player_move();
 
+
+    }
+
+    void player_move(){
+
+        //control
         move_x = Input.GetAxisRaw("Horizontal");
 
+        //animation
+
+        //player direction
+        if(move_x > 0.0f && facing_right == false){
+
+            flip_player();
+
+        }
+        else if(move_x < 0.0f && facing_right == true){
+
+            flip_player();
+
+        }
+        //physics
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (move_x * move_speed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+
+
     }
 
-    public void FixedUpdate(){
+    void flip_player(){
 
-        Vector2 movement = new Vector2(move_x * movement_speed, rigid_body.velocity.y);
+        facing_right = !facing_right;
+        Vector2 local_scale = gameObject.transform.localScale;
+        local_scale.x *= -1;
+        transform.localScale = local_scale;
 
-        rigid_body.velocity = movement;
-        
     }
+
 }
 
