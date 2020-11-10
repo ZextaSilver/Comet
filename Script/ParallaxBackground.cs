c5c9971eb91618c5b72766e168161c09b441a19b
Bowen Zhao <bzhao19@ucsc.edu>
2020-11-09 19:09:51 -0500

    parallax x axis

---
+++ Script/ParallaxBackground.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    public float parallax_effect_multiplier;
+    private Transform camera_transform;
+    private Vector3 last_camera_position;
+
+    private void Start()
+    {
+
+        camera_transform = Camera.main.transform;
+        last_camera_position = camera_transform.position;
+
+    }
+
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+
+        Vector3 delta_movement = camera_transform.position - last_camera_position;
+        //float parallax_effect_multiplier = 0.5f;
+        transform.position += delta_movement * parallax_effect_multiplier;
+        last_camera_position = camera_transform.position;
+
+    }
+}
